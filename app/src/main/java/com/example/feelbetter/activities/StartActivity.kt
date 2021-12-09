593abd0ff96338073907e52ad3270753978181e9
muhammadreza <mra.akhgari@gmail.com>
2021-12-09 21:19:46 +0330

    Add components

--- app/src/main/java/com/example/feelbetter/activities/StartActivity.kt
+++ app/src/main/java/com/example/feelbetter/activities/StartActivity.kt
@@ -1,8 +1,12 @@
 package com.example.feelbetter.activities
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import com.example.feelbetter.R
@@ -16,14 +20,25 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
+            )
+        }
         signUpClick()
         loginClick()
 
 
     }
+
     //listener for clicking signup button that sends the user to signup activity
-    private fun signUpClick(){
-        val button : Button = findViewById(R.id.startSignUp)
+    private fun signUpClick() {
+        val button: Button = findViewById(R.id.startSignUp)
         button.setOnClickListener { // Perform action on click
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
@@ -32,14 +47,13 @@ class StartActivity : AppCompatActivity() {
 
 
     //listener for clicking signup button that sends the user to login activity
-    private fun loginClick(){
-        val button : TextView = findViewById(R.id.startLogin)
+    private fun loginClick() {
+        val button: TextView = findViewById(R.id.startLogin)
         button.setOnClickListener { // Perform action on click
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
-
 
 
 }
