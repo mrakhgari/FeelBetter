8f81c65085f0e89ab9261b7a1424bf045cb99545
muhammadreza <mra.akhgari@gmail.com>
2022-01-04 23:47:28 +0330

    Change the texts

--- app/src/main/java/com/example/feelbetter/datasets/InspiringContentDataset.kt
+++ app/src/main/java/com/example/feelbetter/datasets/InspiringContentDataset.kt
@@ -9,22 +9,22 @@ class InspiringContentDataset {
             InspiringContent(
                 R.string.inspiring_title_1,
                 R.string.inspiring_desc_1,
-                R.drawable.draw_your_dreams
+                R.drawable.dont_allow
             ),
             InspiringContent(
                 R.string.inspiring_title_2,
                 R.string.inspiring_desc_2,
-                R.drawable.i_can_i_do
+                R.drawable.belive
             ),
             InspiringContent(
                 R.string.inspiring_title_3,
                 R.string.inspiring_desc_3,
-                R.drawable.i_can_i_do
+                R.drawable.doing_my_best
             ),
             InspiringContent(
-                R.string.inspiring_title_1,
-                R.string.inspiring_desc_1,
-                R.drawable.draw_your_dreams
+                R.string.inspiring_title_4,
+                R.string.inspiring_desc_4,
+                R.drawable.i_can_i_do
             ),
         )
     }
